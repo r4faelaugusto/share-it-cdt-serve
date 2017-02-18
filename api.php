f9731bc5e6259e0764adce6086a756f9c614bfdb
Rafael Augusto <r4faelaugusto@gmail.com>
2017-02-18 19:07:14 -0300

    Subindo correcao da configuração da api

--- api.php
+++ api.php
@@ -2438,3 +2438,13 @@ class PHP_CRUD_API {
 // 	'database'=>'data/blog.db',
 // ));
 // $api->executeCommand();
+
+$api = new PHP_CRUD_API(array(
+	'dbengine'=>'MySQL',
+	'hostname'=>'localhost',
+    'username'=>'shareit',
+    'password'=>'d*8I(B7zR)[',
+    'database'=>'shareitdb',
+	'charset'=>'utf8'
+));
+$api->executeCommand();
